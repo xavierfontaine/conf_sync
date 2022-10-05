e99616d15c7d3a9859350f0dc1bf79bc0ab13054
Xavier Fontaine <xavier.fontainec@gmail.com>
2022-10-05 12:39:43 +0900

    Fix if clause in sh scripts

--- update_local_from_repo.sh
+++ update_local_from_repo.sh
@@ -3,7 +3,9 @@ cp ./.bashrc $HOME/
 cp ./.profile $HOME/
 cp ./.tmux.conf $HOME/
 if [[ ! -e $HOME/.config/nvim ]]; then
-    mkdir -p $HOME/.config/nvim fi
+    mkdir -p $HOME/.config/nvim
+fi
 if [[ -e $HOME/.bash_profile ]]; then
-    move $HOME/.bash_profile $HOME/.bash_profile.bak fi
+    mv $HOME/.bash_profile $HOME/.bash_profile.bak
+fi
 cp ./init.vim $HOME/.config/nvim/
